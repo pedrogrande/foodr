7cf24556a9629d7d73bb726b0a68d9069225e974
pedroargento <peter.argent@gmail.com>
2013-11-18 19:28:29 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodr::Application.config.secret_key_base = '83bc08b7d2671017c748e9eefc5dfff8b571b4f9273f7464fa89f731f07c7a6214e377d05c6a09f0d250f384faade86138423798d96102b4b5a7ff619b9735d0'
